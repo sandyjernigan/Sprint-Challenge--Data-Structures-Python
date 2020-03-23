0ce0d975707eb884e7fd88a5c7f287f4de15ff29
Sandy <sandy.jernigan@gmail.com>
2020-03-22 20:43:56 -0400

    Difference between 2 lists

--- names/names.py
+++ names/names.py
@@ -13,10 +13,17 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+def difference(list1, list2):
+    # map over each element (x) of combined list to determine if not in one of the lists
+    listDiff = [x for x in list1 + list2 if x not in list1 or x not in list2]
+    return listDiff
+
+duplicates = difference(names_1, names_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
